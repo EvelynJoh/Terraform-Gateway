8b5f51b4a76b63973b04216833a087b32de8b09f
Evelyn <evelyn-johannsen@outlook.de>
2024-01-16 12:46:06 +0100

    Terrafrom - Gateway project

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,13 +1,26 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
-// Lambda function code
-
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
+
   let responseMessage = 'Hello, World!';
+
+  if (event.queryStringParameters && event.queryStringParameters['Name']) {
+    responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
+  }
+
+  // Je nach HTTP-Methode den entsprechenden Endpunkt verarbeiten
+  switch (event.httpMethod) {
+    case 'GET':
+      responseMessage = 'GET /say-hi';
+      break;
+    case 'POST':
+      responseMessage = 'POST /save';
+      break;
+    case 'DELETE':
+      responseMessage = 'DELETE /remove';
+      break;
+    default:
+      responseMessage = 'Unknown endpoint';
+  }
 
   return {
     statusCode: 200,
@@ -15,7 +28,7 @@ module.exports.handler = async (event) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      message: responseMessage,
+      from: responseMessage,
     }),
-  }
-}
+  };
+};
